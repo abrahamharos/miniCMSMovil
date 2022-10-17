c500cab1ebe992a43bc1d31131cba5790e0bdd6e
Mariana Martinez Celis <marianamtzcelis19@gmail.com>
2022-10-17 18:37:38 -0500

    Add card component

--- cmc-front/Feed.js
+++ cmc-front/Feed.js
@@ -1,25 +1,16 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import TextCard from './Cards/TextCard';
+import { NativeBaseProvider } from 'native-base';
 
 const Feed = ({ navigation }) => {
     return (
-        <View style={styles.main}>
+        <NativeBaseProvider>
             <TextCard text='fuera' />
             <TextCard text='fuera' />
             <TextCard text='fuera' />
             <TextCard text='fuera' />
-        </View>
+        </NativeBaseProvider>
     );
 }
 export default Feed;
-
-const styles = StyleSheet.create({
-    main: {
-        display: 'flex',
-        flexDirection: 'column',
-        flex: 1,
-        flexGrow: 1,
-        paddingTop: 50,
-    }
-});
