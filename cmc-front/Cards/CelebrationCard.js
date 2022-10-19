2033db3e04d7b3cbb3539c9fc492c8ac9cc105d3
Daniel Trevino <23410540+danieltre23@users.noreply.github.com>
2022-10-19 13:19:00 -0500

    scroll y confetti

--- cmc-front/Cards/CelebrationCard.js
+++ cmc-front/Cards/CelebrationCard.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Stack, Box, HStack, AspectRatio, Image, Center, Heading, Text } from 'native-base';
 import { Dimensions } from 'react-native';
+import ConfettiCannon from 'react-native-confetti-cannon';
 
-const CelebrationCard = ({ photo, title, author, text, date  }) => {
-    return <Box alignItems="center">
-      <Box width={Dimensions.get('window').width} rounded="lg" overflow="hidden" borderColor="coolGray.200" borderWidth="10" _dark={{
+const CelebrationCard = ({ photo, title, author, text, date }) => {
+  var explosion;
+
+  return <Box alignItems="center">
+    <Box width={Dimensions.get('window').width} rounded="lg" overflow="hidden" borderColor="coolGray.200" borderWidth="10" _dark={{
       borderColor: "coolGray.600",
       backgroundColor: "gray.700"
     }} _web={{
@@ -13,40 +16,41 @@ const CelebrationCard = ({ photo, title, author, text, date  }) => {
     }} _light={{
       backgroundColor: "gray.50"
     }}>
-        <Box>
-          <AspectRatio w="100%" ratio={16 / 9}>
-            <Image source={{
+      <ConfettiCannon count={200} origin={{ x: -10, y: -10 }} ref={ref => (explosion = ref)} onAnimationEnd={() => { explosion && explosion.start(); }} />
+      <Box>
+        <AspectRatio w="100%" ratio={16 / 9}>
+          <Image source={{
             uri: photo
           }} alt="image" />
-          </AspectRatio>
-        </Box>
-        <Stack p="4" space={3}>
-          <Stack space={2}>
-            <Heading size="md" ml="-1">
-              {title}
-            </Heading>
-            <Text fontSize="xs" _light={{
+        </AspectRatio>
+      </Box>
+      <Stack p="4" space={3}>
+        <Stack space={2}>
+          <Heading size="md" ml="-1">
+            {title}
+          </Heading>
+          <Text fontSize="xs" _light={{
             color: "violet.500"
           }} _dark={{
             color: "violet.400"
           }} fontWeight="500" ml="-0.5" mt="-1">
-              {author}
-            </Text>
-          </Stack>
-          <Text fontWeight="400">
-            {text}
+            {author}
           </Text>
-          <HStack alignItems="center" space={4} justifyContent="space-between">
-            <HStack alignItems="center">
-              <Text color="coolGray.600" _dark={{
+        </Stack>
+        <Text fontWeight="400">
+          {text}
+        </Text>
+        <HStack alignItems="center" space={4} justifyContent="space-between">
+          <HStack alignItems="center">
+            <Text color="coolGray.600" _dark={{
               color: "warmGray.200"
             }} fontWeight="400">
-                {date}
-              </Text>
-            </HStack>
+              {date}
+            </Text>
           </HStack>
-        </Stack>
-      </Box>
-    </Box>;
+        </HStack>
+      </Stack>
+    </Box>
+  </Box>;
 };
 export default CelebrationCard;
--- cmc-front/Feed.js
+++ cmc-front/Feed.js
@@ -3,14 +3,16 @@ import { StyleSheet, View, Text } from 'react-native';
 import TextCard from './Cards/TextCard';
 import PhotoCard from './Cards/PhotoCard';
 import CelebrationCard from './Cards/CelebrationCard';
-import { NativeBaseProvider } from 'native-base';
+import { NativeBaseProvider, ScrollView } from 'native-base';
 
 const Feed = ({ navigation }) => {
     return (
         <NativeBaseProvider>
-            <TextCard title='Holiiii' author='Mariana' text='Este es un post de texto de prueba' date='Hoy' />
-            <PhotoCard photo='https://www.northcotevet.com.au/wp-content/uploads/2019/03/tips-puppies-jumping-barking.jpg' title='Holi otra vez' author='Mariana Mtz Celis' text='Este es un post de foto de prueba' date='Ayer' />
-            <CelebrationCard photo='https://avatars.githubusercontent.com/u/25236391?v=4' title='Happy birthday Diego' author='Mariana Mtz' text='Este es un post de celebracion de prueba' date='Hace una semana' />
+            <ScrollView>
+                <TextCard title='Holiiii' author='Mariana' text='Este es un post de texto de prueba' date='Hoy' />
+                <PhotoCard photo='https://www.northcotevet.com.au/wp-content/uploads/2019/03/tips-puppies-jumping-barking.jpg' title='Holi otra vez' author='Mariana Mtz Celis' text='Este es un post de foto de prueba' date='Ayer' />
+                <CelebrationCard photo='https://avatars.githubusercontent.com/u/25236391?v=4' title='Happy birthday Diego' author='Mariana Mtz' text='Este es un post de celebracion de prueba' date='Hace una semana' />
+            </ScrollView>
         </NativeBaseProvider>
     );
 }
