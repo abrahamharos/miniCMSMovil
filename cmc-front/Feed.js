8fcd6bde7b12065ccfd899435ebce1a3df2d6d51
Ernesto García <ernesto_garciat@outlook.com>
2022-10-21 17:17:04 -0500

    Feed cards request and api fixes

--- cmc-front/Feed.js
+++ cmc-front/Feed.js
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import TextCard from './Cards/TextCard';
 import PhotoCard from './Cards/PhotoCard';
 import CelebrationCard from './Cards/CelebrationCard';
 import { NativeBaseProvider, ScrollView } from 'native-base';
+import { API_KEY } from '@env';
+import axios from "axios";
+
 
 const Feed = ({ navigation }) => {
+    const [cards, setCards] = useState([]);
+
+    useEffect(() => {
+        async function getPost() {
+        const response = await axios.get('https://6qeaxhu5lk.execute-api.us-east-1.amazonaws.com/default/getPosts', { 'headers': { 'Content-Type': 'application/json', 'x-api-key': API_KEY } })
+        if(response.status == 200){
+            response.data.map(card =>{
+                let date = new Date(card.timestamp * 1000);
+                card.timestamp = date.toLocaleDateString("en-GB");
+            })
+            setCards(response.data);
+        }
+        }
+        const interval = setInterval(() => {
+            getPost();
+        }, 1000);
+        return () => clearInterval(interval);
+    }, []);
+
     return (
         <NativeBaseProvider>
             <ScrollView>
-                <TextCard title='Holiiii' author='Mariana' text='Este es un post de texto de prueba' date='Hoy' />
-                <PhotoCard photo='https://www.northcotevet.com.au/wp-content/uploads/2019/03/tips-puppies-jumping-barking.jpg' title='Holi otra vez' author='Mariana Mtz Celis' text='Este es un post de foto de prueba' date='Ayer' />
-                <CelebrationCard photo='https://avatars.githubusercontent.com/u/25236391?v=4' title='Happy birthday Diego' author='Mariana Mtz' text='Este es un post de celebracion de prueba' date='Hace una semana' />
+                {cards.map(card => {
+                    if(card.type == "celebration") {
+                        return <CelebrationCard photo={card.img} title={card.title} author={card.author} text={card.message} date={card.timestamp} />
+                    } else if (card.type == "text") {
+                        return <TextCard title={card.title} author={card.author} text={card.message} date={card.timestamp} />
+                    } else if (card.type == "photo") {
+                        return <PhotoCard photo={card.img} title={card.title} author={card.author} text={card.message} date={card.timestamp} />
+                    }
+                })}
             </ScrollView>
         </NativeBaseProvider>
     );
